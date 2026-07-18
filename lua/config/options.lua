9f6aef655955c8816cb8c5030be07283fa75f755
_amlq <kuba.janor@gmail.com>
2026-07-18 02:30:51 +0200

    fix spellcheck

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,5 +25,5 @@ opt.wrap = false          -- Don't wrap long lines automatically
 opt.mouse = "a"           -- Enable mouse support in all modes
 
 -- lang
-opt.spellang = {'eng', 'pl'}
+opt.spelllang = {'eng', 'pl'}
 opt.spell = true
